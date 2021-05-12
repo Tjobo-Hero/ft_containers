99ecdf159e668ff2d703a6bf76859620125c2596
Tim van Citters <timvcitters@gmail.com>
2021-05-12 16:53:36 +0200

    check

---
+++ play_around/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: timvancitters <timvancitters@student.co      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/05/12 16:53:11 by timvancitte   #+#    #+#                 */
+/*   Updated: 2021/05/12 16:53:25 by timvancitte   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <map>
+
+int main ()
+{
+  std::map<char,int> mymap;
+
+  mymap['a']=10;
+  mymap['b']=20;
+  mymap['c']=30;
+
+  std::pair<std::map<char,int>::iterator,std::map<char,int>::iterator> ret;
+  ret = mymap.equal_range('b');
+
+  std::cout << "lower bound points to: ";
+  std::cout << ret.first->first << " => " << ret.first->second << '\n';
+
+  std::cout << "upper bound points to: ";
+  std::cout << ret.second->first << " => " << ret.second->second << '\n';
+
+  return 0;
+}
